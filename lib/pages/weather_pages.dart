410207a57394790624910bca519ee0fdea26416f
William Pettersen <williamkpettersen@outlook.com>
2024-10-06 14:54:46 +1100

    geolocations added

---
+++ lib/pages/weather_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherStatePage();
+}
+
+class _WeatherStatePage extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/services/weather_service.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:geolocator/geolocator.dart';
+
+import '../models/weather_models.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+
+  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http.get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
+  }
+}
